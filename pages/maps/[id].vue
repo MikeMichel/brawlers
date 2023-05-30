9f775283d7d98171314b01dd211a3a8ed9c4c93e
mike.michel <mike.michel811@gmail.com>
2023-05-30 10:21:52 +0200

    [main] Added pages

---
+++ pages/maps/[id].vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { Maps } from '~~/types'
+import { provideContext } from 'nuxt'
+import { brawlers } from '~/pages/index.vue'
+
+provideContext('brawlers', brawlers)
+
+const route = useRoute()
+console.log(route.params)
+const { data: map, pending, error } = await useFetch<Maps>(`https://api.brawlapi.com/v1/maps/${route.params.id}`)
+
+useHead({
+	title: `${map.value?.title} | Maps`,
+})
+</script>
+
+<template>
+	<section class="dark:bg-gray-800 dark:text-gray-100">
+		<div class="container flex flex-col-reverse mx-auto lg:flex-row">
+			<div
+				class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-violet-400 dark:text-gray-900">
+				<div class="flex space-x-2 sm:space-x-4">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+						class="flex-shrink-0 w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z">
+						</path>
+					</svg>
+					<div v-if="pending" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+						Loading ...
+					</div>
+					<div v-else-if="error">
+						{{ error }}
+					</div>
+					<div class="space-y-2">
+						<p class="text-lg font-medium leading-snug"> {{ map.name }}</p>
+						<p class="leading-snug">{{ map.description }}</p>
+					</div>
+				</div>
+			</div>
+			<div class="lg:w-1/2 xl:w-3/5 dark:bg-gray-800">
+				<div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
+					<img :src="map.imageUrl" .alt="map.name"
+						class="rounded-lg shadow-lg dark:bg-gray-500">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="overflow-x-auto w-full">
+  <table class="table w-full">
+    <!-- head -->
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" class="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row 1 -->
+      <tr v-for="best in map.stats" :key="map.stats.brawler">
+        <td>
+          <div class="flex items-center space-x-3">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div class="font-bold">{{ best.brawler }}</div>
+              <!-- <div class="text-sm opacity-50">{{ brawlers.id[0].name }}</div> -->
+
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br/>
+          <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button class="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    </tbody>
+  </table>
+</div>
+	</section>
+</template>
--- pages/maps/index.vue
+++ pages/maps/index.vue
@@ -6,12 +6,86 @@ const { data: maps } = await useFetch('https://api.brawlapi.com/v1/maps');
 const search = ref('')
 const searchResults = computed(() => {
   return maps.value.list.filter((map) => {
-    return maps.name.toLowerCase().includes(search.value.toLowerCase())
+    return map.name.toLowerCase().includes(search.value.toLowerCase())
   })
+})
+
+const map_types = [
+  'All',
+  'Gem Grab',
+  'Brawl Ball',
+  'Showdown',
+  'Bounty',
+  'Heist',
+  'Siege',
+  'Hot Zone',
+  'Knockout',
+  'Power Play',
+  'Big Game',
+  'Robo Rumble',
+  'Boss Fight',
+  'Championship Challenge',
+  'Special Events',
+  'Training Cave',
+  'Map Maker'
+]
+
+// computed property to filter the maps by map_type,name and map_type
+const type = ref('All')
+const filteredMaps = computed(() => {
+  console.log('search: ', search.value)
+  console.log('map_type: ', type.value)
+  console.log('maps: ', maps.value)
+  const filtered = maps.value.list.filter((map) => {
+    console.log('map: ', map)
+    const result = (type.value === 'All' || map.gameMode.name === type.value) && map.name.toLowerCase().includes(search.value.toLowerCase())
+    console.log('result: ', result)
+    return result
+  })
+  console.log('filtered: ', filtered)
+  return filtered
 })
 </script>
 
 <template>
-maps
-</template>
+  <div class="text-center py-10 md:max-w-xl md:mx-auto">
+    <h1 class="text-3xl mb-3 mt-2 font-semibold tracking-normal text-gray-800">
+      Maps
+    </h1>
+    <h2 class="text-lg mb-8 font-normal text-gray-600 dark:text-gray-400">
+      Yay, all maps from Brawl Stars!
+    </h2>
+  </div>
 
+<div class="p-4 xl:p-0">
+  <input
+    v-model="search"
+    small="true"
+    type="text"
+    placeholder="Search maps..."
+    class="w-1/2 px-4 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+  />
+  <select v-model="type" class="w-1/2 px-4 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline">
+    <option v-for="item in map_types" :key="item" :value="item">
+      {{ item }}
+    </option>
+  </select>
+</div>
+
+<div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <NuxtLink v-for="map in filteredMaps" :key="map.id"
+      class="bg-white flex flex-col rounded-lg transition duration-300"
+      :to="`/maps/${map.id}`"
+      :title="map.name"
+    >
+    <div class="card h-min w-full bg-base-100 shadow-xl">
+    <!-- <div class="card w-96 bg-base-100 shadow-xl"> -->
+    <figure><img :src="map.imageUrl" .alt="map.name" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ map.name }}</h2>
+      <h4 class="badge md:badge-md badge-secondary">{{map.gameMode.name}}</h4>
+    </div>
+  </div>
+    </NuxtLink>
+  </div>
+</template>
