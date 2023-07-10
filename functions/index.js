3340fa9c2b1232818aa0a286370ca1707cf3f177
mike.michel <mike.michel811@gmail.com>
2023-07-10 14:57:41 +0200

    [main] Added index.js

---
+++ functions/index.js
@@ -0,0 +1,6 @@
+// Description: This file is the entry point for all the functions.
+// It imports the functions from their respective submodules and exports them.
+// https://firebase.google.com/docs/functions/organize-functions?hl=en&gen=2nd#write_functions_in_multiple_files
+import * as fetchDataAndSaveToFirestore from "./fetchDataAndSaveToFirestore";
+exports.fetchDataAndSaveToFirestore =
+  fetchDataAndSaveToFirestore.fetchDataAndSaveToFirestore;
